3c3f54130b0d828f9cc5d8d283c290230dfad2a5
Dalton Smith <gamefollower26@gmail.com>
2018-01-12 20:05:35 -0500

    Initial Commit

---
+++ src/org/usfirst/frc/team66/robot/Constants.java
@@ -0,0 +1,7 @@
+package org.usfirst.frc.team66.robot;
+
+/* Only variables that have a fixed (never changing) value should be put here */
+
+public class Constants {
+
+}
---
+++ src/org/usfirst/frc/team66/robot/Drivetrain.java
@@ -0,0 +1,5 @@
+package org.usfirst.frc.team66.robot;
+
+public class Drivetrain {
+
+}
