f881d600b665d51bf741d137e37ad58459298308
fayfai64 <fayfai64@gmail.com>
2018-01-18 20:32:20 -0500

    Added Constants

--- src/org/usfirst/frc/team66/robot/Constants.java
+++ src/org/usfirst/frc/team66/robot/Constants.java
@@ -1,7 +1,15 @@
 package org.usfirst.frc.team66.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
+
 /* Only variables that have a fixed (never changing) value should be put here */
 
 public class Constants {
-
+	Joystick DriveController1 = new Joystick(0);
+	Joystick OperatorController2 = new Joystick(1);
+	
+	LeftJoyPosY = DriveController.getRawAxis(0);
+	
+	
+	
 }
--- src/org/usfirst/frc/team66/robot/Drivetrain.java
+++ src/org/usfirst/frc/team66/robot/Drivetrain.java
@@ -1,5 +1,16 @@
 package org.usfirst.frc.team66.robot;
 
-public class Drivetrain {
+import edu.wpi.first.wpilibj.Joystick;
 
+public class Drivetrain {
+	int joyLX = Constants.LeftJoyX;
+	int joyLY = Constants.LeftJoyY;
+	int joyRX = Constants.RightJoyX;
+	int joyRY = Constants.RightJoyY;
+	
+	Joystick DriveController1 = new Joystick(0);
+	Joystick OperatorController2 = new Joystick(1);
+	
+	
+	
 }
